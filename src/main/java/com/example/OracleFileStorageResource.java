8c2029fd704c061a9b3561ebd62a5224d448d12c
kailash kannedari <kailashkannedari@gmail.com>
2023-11-02 16:02:59 +0530

    second commit

--- src/main/java/com/example/OracleFileStorageResource.java
+++ src/main/java/com/example/OracleFileStorageResource.java
@@ -4,6 +4,7 @@ import com.oracle.bmc.ClientConfiguration;
 import com.oracle.bmc.Region;
 import com.oracle.bmc.auth.AuthenticationDetailsProvider;
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
+import com.oracle.bmc.objectstorage.ObjectStorage;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
 import com.oracle.bmc.objectstorage.requests.GetObjectRequest;
 import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
@@ -15,8 +16,11 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import java.io.*;
+import java.nio.file.Files;
 
 
 @Path("/files")
@@ -40,50 +44,39 @@ public class OracleFileStorageResource{
     @ConfigProperty(name = "file.privateKeyPath")
     String privateKeyPath;
 
+
     @POST
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<String> uploadFile(@FormParam("fileName") InputStream file1)
+    public Uni<String> uploadFile(@RestForm("file") FileUpload file)
     {
+
+
+
+        String passphrase = "";
         return Uni.createFrom().item(() -> {
-            String passphrase = "";
+
             System.setProperty("OCI_JAVASDK_JERSEY_CLIENT_DEFAULT_CONNECTOR_ENABLED","true");
+            //ObjectStorageClient  client = new ObjectStorageClient(getAuthenticationDetailsProvider(passphrase), getClientConfiguration());
+            ObjectStorage client = ObjectStorageClient.builder().configuration(getClientConfiguration()).build(getAuthenticationDetailsProvider(passphrase));
 
-            AuthenticationDetailsProvider authBuilder =
-                    SimpleAuthenticationDetailsProvider.builder()
-                            .userId(userId)
-                            .fingerprint(fingerprint)
-                            .tenantId(tenantId)
-                            .privateKeySupplier(() -> {
-                                File file = new  File(privateKeyPath);
-                                try {
-                                    return new FileInputStream(file);
-                                } catch (FileNotFoundException e) {
-                                    return getClass().getResourceAsStream(privateKeyPath);
-                                }
-                            })
-                            .passPhrase(passphrase)
-                            .region(Region.fromRegionId(region))
-                            .build();
+            byte[] fileBytes;
+            try {
+                fileBytes = Files.readAllBytes(file.filePath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            InputStream inputStream = new ByteArrayInputStream(fileBytes);
 
-            ClientConfiguration clientConfig
-                    = ClientConfiguration.builder()
-                    .connectionTimeoutMillis(3000)
-                    .readTimeoutMillis(60000)
-                    .build();
-            ObjectStorageClient client = new ObjectStorageClient(authBuilder, clientConfig);
-
-            InputStream f = file1;
-
-            String objectName = "examples2.txt";
+            String objectName = file.fileName();
 
             PutObjectRequest request = PutObjectRequest.builder()
                     .bucketName(bucketName)
                     .objectName(objectName)
                     .namespaceName(namespace)
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .putObjectBody(f)
+                    .putObjectBody(inputStream)
                     .build();
 
             PutObjectResponse response = client.putObject(request);
@@ -96,55 +89,66 @@ public class OracleFileStorageResource{
     }
 
 
+
     @GET
     @Path("/download/{fileName}")
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> DownloadFile(@PathParam("fileName") String fileName)
     {
-
+            String passphrase = "";
             return Uni.createFrom().item(() -> {
-                String passphrase = "";
                 System.setProperty("OCI_JAVASDK_JERSEY_CLIENT_DEFAULT_CONNECTOR_ENABLED","true");
-
-                AuthenticationDetailsProvider authBuilder =
-                        SimpleAuthenticationDetailsProvider.builder()
-                                .userId(userId)
-                                .fingerprint(fingerprint)
-                                .tenantId(tenantId)
-                                .privateKeySupplier(() -> {
-                                    File file = new  File(privateKeyPath);
-                                    try {
-                                        return new FileInputStream(file);
-                                    } catch (FileNotFoundException e) {
-                                        return getClass().getResourceAsStream(privateKeyPath);
-                                    }
-                                })
-                                .passPhrase(passphrase)
-                                .region(Region.fromRegionId(region))
-                                .build();
-
-                ClientConfiguration clientConfig
-                        = ClientConfiguration.builder()
-                        .connectionTimeoutMillis(3000)
-                        .readTimeoutMillis(60000)
-                        .build();
-                ObjectStorageClient client = new ObjectStorageClient(authBuilder, clientConfig);
+                ObjectStorageClient client = new ObjectStorageClient(getAuthenticationDetailsProvider(passphrase), getClientConfiguration());
                 GetObjectRequest request = GetObjectRequest.builder()
                         .bucketName(bucketName)
                         .namespaceName(namespace)
                         .objectName(fileName)
                         .build();
 
-
                 GetObjectResponse response = client.getObject(request);
                 InputStream fileInputStream = response.getInputStream();
                 Response.ResponseBuilder responseBuilder = Response.ok(fileInputStream);
-                responseBuilder.header("Content-Disposition", "attachment; fileName=examples.txt");
+
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
                 return responseBuilder.build();
-            }).onFailure().recoverWithItem(e -> Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("File download failed: " + e.getMessage()).build());
+            }).onFailure().recoverWithItem(e -> Response.status(Response.Status.INTERNAL_SERVER_ERROR).build());
     }
+
+    private AuthenticationDetailsProvider getAuthenticationDetailsProvider(String passphrase) {
+        AuthenticationDetailsProvider authBuilder =
+                SimpleAuthenticationDetailsProvider.builder()
+                        .userId(userId)
+                        .fingerprint(fingerprint)
+                        .tenantId(tenantId)
+                        .privateKeySupplier(() -> {
+                            File f = new  File(privateKeyPath);
+                            try {
+                                return new FileInputStream(f);
+                            } catch (FileNotFoundException e) {
+                                return getClass().getResourceAsStream(privateKeyPath);
+                            }
+                        })
+                        .passPhrase(passphrase)
+                        .region(Region.fromRegionId(region))
+                        .build();
+        return authBuilder;
+    }
+
+    private static ClientConfiguration getClientConfiguration() {
+        ClientConfiguration clientConfig
+                = ClientConfiguration.builder()
+                .connectionTimeoutMillis(3000)
+                .readTimeoutMillis(60000)
+                .build();
+        return clientConfig;
+    }
+
+
 }
 
 
